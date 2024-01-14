718ce5b0f82fe84ec1a8dac9bae39d9a10359fd5
sagarbparhad <156337682+sagarbparhad@users.noreply.github.com>
2024-01-14 20:34:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text", {
+    strings: ["Web Developer" , "Software Developer" , "Frontend Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
